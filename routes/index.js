758469d420e486309bbf308af0e1e2f93068d4a1
xiaoming <228160255@qq.com>
2016-06-23 20:59:58 +0800

    d

--- routes/index.js
+++ routes/index.js
@@ -4,65 +4,61 @@ var upload = require('../dao/upload');
 var formidable = require('formidable');
 var fs = require('fs');
 
-router.get('/', function(req, res, next) {
-    upload.findAll(function(result){
+router.get('/', function (req, res, next) {
+    upload.findAll(function (result) {
         res.render('show');
     })
 });
 
-router.get('/show', function(req, res, next) {
-    upload.findAll(function(result){
+router.get('/show', function (req, res, next) {
+    upload.findAll(function (result) {
         res.json(result);
     })
 });
 
-router.post('/upload', function(req, res, next){
-    var form = new formidable.IncomingForm(req),docs=[];;
+router.post('/upload', function (req, res, next) {
+    var form = new formidable.IncomingForm(req), docs = [];
     var url;
-    
     form.uploadDir = 'tmp/';
     form.parse(req);
-    form.on('file', function(field, file) {
+    form.on('file', function (field, file) {
         var date = new Date();
         var ms = Date.parse(date);
         docs.push(file);
-        url = "/alidata1/admin/xiaoming/FirstNodeApp/tmp/files" + ms + '_'+file.name;
-	fs.renameSync(file.path, url);
-    }).on('end', function() {
-        var responseData={
+        url = "/alidata1/admin/xiaoming/FirstNodeApp/tmp/files" + ms + '_' + file.name;
+        fs.renameSync(file.path, url);
+    }).on('end', function () {
+        var responseData = {
             success: true,
             url: url
         };
         res.send(responseData);
-    }).on('error', function(err){
-	console.log(err);
-});
+    }).on('error', function (err) {
+        console.log(err);
+    });
 });
 
-router.post('/submit', function(req, res, next){
-    var form = new formidable.IncomingForm(req),param = {};
+router.post('/submit', function (req, res, next) {
+    var form = new formidable.IncomingForm(req), param = {};
     //存放目录
     form.parse(req);
 
-    form.on('field', function(field, value) {
+    form.on('field', function (field, value) {
         param[field] = value;
-    }).on('end', function(){
+    }).on('end', function () {
         param['createdAt'] = new Date();
         param['updatedAt'] = new Date();
-        upload.insert(param, function(){
+        upload.insert(param, function () {
             res.redirect("/")
         });
     });
 });
 
-router.get('/download', function(req, res, next){
+router.get('/download', function (req, res, next) {
     var downloadUrl = req.query.url;
-    res.download(downloadUrl, function(err){
-        if(err){
-		console.log(err);
-            console.log('download err url' + downloadUrl);
+    res.download(downloadUrl, function (err) {
+        if (err) {
             console.log(err);
-            console.log('download err url ' + downloadUrl);
             res.end("下载失败!");
         }
     });
